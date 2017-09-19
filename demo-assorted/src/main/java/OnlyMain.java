8a19e6a1722a32c294bf34c68ca9fb1194b5e999
epcm <epcm@EPCM-MacBook-Pro.local>
2017-09-19 14:51:07 +0800

    20170919:15

---
+++ demo-assorted/src/main/java/OnlyMain.java
@@ -0,0 +1,17 @@
+/**
+ * Created by mujiang on 2017/9/18.
+ */
+public class OnlyMain {
+
+
+    public static void main(String[] args){
+
+        String requestURI = "/pcsd-newretail-ac/ddddd";
+
+        if(requestURI.contains("/v1/users/") || requestURI.contains("pcsd-newretail-ac"))
+            System.out.print("pass");
+
+
+
+    }
+}
